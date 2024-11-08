27793abd2c7e211620cbb3f1cb8c9995bd5a6de7
Dhrubub <dhruvjobanputra8@gmail.com>
2024-11-08 14:17:05 +0800

    redirect to pdf

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,15 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import resume from '../assets/Dhruv_Resume.pdf';
 
 const Resume = () => {
-	return (
-		<div className='h-screen flex flex-col'>
-			<iframe
-				src={resume}
-				className='h-full w-full'
-				title="Dhruv's Resume"
-			/>
-		</div>
-	);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		// Open the PDF in the same tab
+		window.open(resume, '_self');
+		navigate('/', { replace: true });
+	}, [navigate]);
+
+	return null; // No UI needed since we're opening the PDF
 };
 
 export default Resume;
